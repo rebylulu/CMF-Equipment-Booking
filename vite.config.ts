cfc171cfcf733589fdbb51b93dc3df1d02662938
Vida Huang <vidahuang@logifvfjg1nyq6l7.logitech.com>
2025-07-22 16:17:09 +0800

    Fix Vite base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   // https://github.com/rebylulu/CMF-Equipment-Booking.git
-  base: '/booking-platform-vite/',
+  base: '/CMF-Equipment-Booking/',
   build: {
     outDir: 'dist',
   },
